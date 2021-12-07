6d2978b67294404f0cef5f019b4b7f30c5462bc8
tegarpryd <60976057+tegarpryd@users.noreply.github.com>
2021-12-07 18:48:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 TOKEN = os.environ.get("TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
-OWNER = os.environ.get("OWNER", "kenkanasw")
-GROUP = os.environ.get("GROUP", "musikkugroup")
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
+ADMIN = int(os.environ.get("ADMIN", "TegarPrayudaa"))
+OWNER = os.environ.get("OWNER", "tegarprayudaaaa")
+GROUP = os.environ.get("GROUP", "CariKenalanBebas")
+CHANNEL = os.environ.get("CHANNEL", "ChatBot_Channel")
 PROJECT_NAME = os.environ.get("PROJECT_NAME", "PRIVATE CHAT")
